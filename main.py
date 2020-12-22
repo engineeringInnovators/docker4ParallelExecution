a60a8ea43f14485398fc758a906d65fa138f43f6
Ahamed Thomsheer <thomsheer.ahamed@sap.com>
2020-12-22 16:29:23 +0530

    testing

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def prepare_results_report(container):
     container_object.remove(v=False)
     container_name = container_name[:-3]
     result_folder = os.path.join(container_volume,'results')
-    print("is " + result_folder + " a directory - " + os.path.isdir(result_folder))
+    # print("is " + result_folder + " a directory - " + os.path.isdir(result_folder))
     if os.path.isdir(result_folder):
         new_results_name = os.path.join(main_folder_path,container_name)
         print("new_results_name: " + new_results_name)
@@ -102,6 +102,8 @@ def prepare_results_report(container):
             for file in [os.path.join(root, f) for f in files]:
                 os.chown(file, 1000, 1000)
                 os.chmod(file, 0o644)
+    else:
+        print("is " + result_folder + " a directory - " + os.path.isdir(result_folder))
 def build_metadata(total, starttime, endtime, inprogress):
     # metadata_file = os.path.join(main_folder_path,'metadata.json')
     # metadata_file = '/home/ccloud/reports/server/metadata.json'
