b999574602a5f6b0b3d32116522b26089221fc67
Ahamed Thomsheer <thomsheer.ahamed@sap.com>
2021-01-19 12:41:42 +0530

    baseurl test

--- main.py
+++ main.py
@@ -69,22 +69,13 @@ def get_config_file():
             if filepath.endswith(search_config):
                 config_path = os.path.join(root_dir, filename)
                 shutil.copy(filepath, config_path)
-
-                with open(config_path) as json_file:
-                    _data = json_file.read()
-                    obj = _data[_data.find('{') : _data.rfind('}')+1]
-                    _data = json.load(obj)
-                    json_file.close()
-                print("json___________________________")
-                print(_data)
-                print("--------------------------")
                 with open(config_path) as config_file:
                     _lines = config_file.readlines()
                     config_file.close()
                 print("-----------------base------------------------")
                 print(_lines)
                 for line in _lines:
-                    if('baseUrl' in line):
+                    if('baseUrl' in line and not('//' in line)):
                         print(line)
                 print("-----------------------------------------")
 
