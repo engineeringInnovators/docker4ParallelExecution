7d0c3e1803e21a0ece2a4326887a6027a1d6a4fc
Ahamed Thomsheer <thomsheer.ahamed@sap.com>
2021-02-11 17:21:24 +0530

    email testing

--- email/email.py
+++ email/email.py
@@ -23,58 +23,63 @@ output_dir = os.path.join(root_dir, "email" + os.sep + "output")
 print("output_dir: "+output_dir)
 
 try:
-  for subdir, dirs, files in os.walk(output_dir):
-    # print(files.count())
-    for file in files:
-      print(file)
-      file_path = os.path.join(output_dir, file)
-      print(file_path)
-      os.remove(file_path)
+    for subdir, dirs, files in os.walk(output_dir):
+        # print(files.count())
+        for file in files:
+            print(file)
+            file_path = os.path.join(output_dir, file)
+            print(file_path)
+            os.remove(file_path)
+
+    if args.filename:
+        with open("email/template.html", "r", encoding='utf-8') as f:
+            text = f.read()
+            f.close()
+
+        meta_data = os.path.join(reports_dir, 'metadata.json')
+
+        if os.path.isfile(meta_data):
+            with open(meta_data) as json_file:
+                meta = json.load(json_file)
+                json_file.close()
+
+            if(meta['latest']):
+                file_structure = os.path.join(
+                    reports_dir, 'fileStructure.json')
+
+                if os.path.isfile(file_structure):
+                    with open(file_structure) as json_file:
+                        latest = json.load(json_file)
+                        json_file.close()
+
+                    formated_date = str(meta['latest']).replace(
+                        ".", "").replace(" ", "").replace(":", "")
+                    # print(formated_date)
+
+                    for key, value in latest["files"].items():
+                        # print(key.replace(".", "").replace(" ", ""))
+                        if str(formated_date) == str(key).replace(".", "").replace(" ", "").replace(":", ""):
+                            data = value["totalCounts"]
+                            # print(data)
+                            text = text.replace("{{TOTAL_TIME}}", str(data['totalExecutionTime'])).replace(
+                                "{{TOTAL_SPECS}}", str(data['total'])).replace(
+                                "{{TOTAL_PASSED}}", str(data['passed'])).replace(
+                                "{{TOTAL_FAILED}}", str(data['failed']))
+
+                            text = text.replace("{{BASE_URL}}", str(
+                                meta[formated_date]['baseUrl'])).replace(" ", "").replace("\n", "")
+                            # print("args.filename: " +args.filename)
+                            try:
+                                file = open("email/output/" +
+                                            args.filename+".txt", "w")
+                                file.write(text)
+                                # print(text)
+                                file.close()
+                            except OSError:
+                                print("error creating file")
+                            break
+
 except OSError:
-  print("Error while deleting text files")
-
-if args.filename:
-    with open("email/template.html", "r", encoding='utf-8') as f:
-        text = f.read()
-        f.close()
-
-    meta_data = os.path.join(reports_dir, 'metadata.json')
-
-    if os.path.isfile(meta_data):
-        with open(meta_data) as json_file:
-            meta = json.load(json_file)
-            json_file.close()
-
-        if(meta['latest']):
-            file_structure = os.path.join(reports_dir, 'fileStructure.json')
-
-            if os.path.isfile(file_structure):
-                with open(file_structure) as json_file:
-                    latest = json.load(json_file)
-                    json_file.close()
-
-                formated_date = str(meta['latest']).replace(
-                    ".", "").replace(" ", "").replace(":", "")
-                # print(formated_date)
-
-                for key, value in latest["files"].items():
-                    # print(key.replace(".", "").replace(" ", ""))
-                    if str(formated_date) == str(key).replace(".", "").replace(" ", "").replace(":", ""):
-                        data = value["totalCounts"]
-                        # print(data)
-                        text = text.replace("{{TOTAL_TIME}}", str(data['totalExecutionTime'])).replace(
-                            "{{TOTAL_SPECS}}", str(data['total'])).replace(
-                            "{{TOTAL_PASSED}}", str(data['passed'])).replace(
-                            "{{TOTAL_FAILED}}", str(data['failed']))
-
-                        text = text.replace("{{BASE_URL}}", str(
-                            meta[formated_date]['baseUrl'])).replace(" ", "").replace("\n", "")
-                        # print("args.filename: " +args.filename)
-                        try:
-                          file = open("email/output/"+args.filename+".txt", "w")
-                          file.write(text)
-                          # print(text)
-                          file.close()
-                        except OSError:
-                            print("error creating file")
-                        break
+    print("Error while deleting text files")
+else:
+    print("error")
